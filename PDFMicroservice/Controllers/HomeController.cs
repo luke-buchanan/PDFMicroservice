434096fbac22910c6fdd0dfdfeebd0546b6f74cb
gtaylor038 <graeme.taylor@hotmail.com>
2022-02-03 11:19:28 +0000

    removed iLogger

--- PDFMicroservice/Controllers/HomeController.cs
+++ PDFMicroservice/Controllers/HomeController.cs
@@ -6,13 +6,6 @@ namespace PDFMicroservice.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
