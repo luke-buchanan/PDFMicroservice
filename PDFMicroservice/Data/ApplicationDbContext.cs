add42977c3d904b3a51c6875208676bbcacea418
gtaylor038 <graeme.taylor@hotmail.com>
2022-02-03 11:57:39 +0000

    create PDF model

--- PDFMicroservice/Data/ApplicationDbContext.cs
+++ PDFMicroservice/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using PDFMicroservice.Models;
 
 namespace PDFMicroservice.Data;
 
@@ -9,4 +10,6 @@ public class ApplicationDbContext : IdentityDbContext
         : base(options)
     {
     }
+
+    public DbSet<PdfModel> PdfModels { get; set; }
 }
---
+++ PDFMicroservice/Models/PdfModel.cs
@@ -0,0 +1,7 @@
+namespace PDFMicroservice.Models;
+
+public class PdfModel
+{
+    public int Id { get; set; }
+    public string TextInput { get; set; }
+}
