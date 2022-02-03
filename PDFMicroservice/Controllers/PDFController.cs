76d3a9dff5bf900ffb40c0d236ac25622f922ec3
gtaylor038 <graeme.taylor@hotmail.com>
2022-02-03 15:52:05 +0000

    added functionality for saving and retrieving pdfs

--- PDFMicroservice/Controllers/PDFController.cs
+++ PDFMicroservice/Controllers/PDFController.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using PDFMicroservice.Data;
 using PDFMicroservice.Models;
 using PDFMicroservice.ViewModels;
+using PdfSharpCore.Drawing;
+using PdfSharpCore.Pdf;
 
 namespace PDFMicroservice.Controllers
 {
@@ -10,7 +13,8 @@ namespace PDFMicroservice.Controllers
     {
 
         private ApplicationDbContext _dbContext;
-
+        private readonly string FilePath = "/Users/gtaylor038/Downloads/";
+        
         public PDFController(ApplicationDbContext dbContext)
         {
             _dbContext = dbContext;
@@ -23,7 +27,7 @@ namespace PDFMicroservice.Controllers
         }
         
         [HttpGet("/api/{id}")]
-        public IActionResult Details(int id)
+        public async Task<IActionResult> Details(int id)
         {
             var pdf = _dbContext.PdfModels.Find(id);
             if(pdf == null)
@@ -32,7 +36,14 @@ namespace PDFMicroservice.Controllers
             }
             else
             {
-                return Ok(pdf);
+                var memory = new MemoryStream();
+                using (var stream = new FileStream($"{FilePath}{id}.pdf", FileMode.Open))
+                {
+                    await stream.CopyToAsync(memory);
+                }
+                memory.Position = 0;
+
+                return File(memory, "application/pdf");
             }
         }
         
@@ -42,14 +53,27 @@ namespace PDFMicroservice.Controllers
             var pdf = new PdfModel
             {
                 TextInput = viewModel.TextInput,
-                FilePath = "local-host://8080" 
+                FilePath = "local-host://8080",
             };
-            
             
             _dbContext.PdfModels.Add(pdf);
             _dbContext.SaveChanges();
             
+            GeneratePdf(viewModel.TextInput);
             return RedirectToAction("Details", "PDF");
+        }
+
+        public void GeneratePdf(string text)
+        {
+            PdfDocument document = new PdfDocument();
+            PdfPage page = document.AddPage();
+            XGraphics gfx = XGraphics.FromPdfPage(page);
+            XFont font = new XFont("Calibri", 20, XFontStyle.Bold);
+            
+            gfx.DrawString(text, font, XBrushes.Black, new XRect(0, 0, page.Width, page.Height), XStringFormats.Center);
+
+            // string filename = "pdfGenerator.pdf";
+            document.Save($"{FilePath}12.pdf");
         }
     }
 }
